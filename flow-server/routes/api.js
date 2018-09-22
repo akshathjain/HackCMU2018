77ceff1f28a7164df5b3a21007256a079aec224f
Ashwin Srinivasan <fishdev@users.noreply.github.com>
2018-09-21 21:01:46 -0400

    Added /api/flow endpoint to post data

--- flow-server/routes/api.js
+++ flow-server/routes/api.js
@@ -1,3 +1,4 @@
+const createError = require('http-errors');
 const express = require('express');
 
 const router = express.Router();
@@ -8,8 +9,28 @@ const router = express.Router();
     value (should be an int or float)
   Timestamp is recorded on the server
 */
-router.get('/flow', (req, res) => {
-  res.send('respond with a resource');
+
+router.post('/flow', (req, res, next) => {
+  const { id } = req.query;
+  const { value } = req.body;
+
+  // make sure we have valid params
+  if (!id || !value || Number.isNaN(value)) {
+    next(createError(400, 'Provide a value Flow ID and data value'));
+  }
+
+  // check for Flow membership in db
+  req.db.sismember('flows', id, (err, reply) => {
+    if (err) return next(createError(500, err));
+    if (!reply) return next(createError(404, 'Flow not found'));
+
+    // insert data into sorted set for this Flow (include timestamp)
+    const timestamp = Date.now();
+    req.db.zadd([`flow:${id}`, timestamp, value], (addErr) => {
+      if (addErr) return next(createError(500, err));
+      res.status(200).send(`Recorded data for Flow ${id} at ${timestamp}`);
+    });
+  });
 });
 
 module.exports = router;

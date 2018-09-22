c00a1396f786d8edecd8a765aee367a9266b9cb6
Ashwin Srinivasan <fishdev@users.noreply.github.com>
2018-09-22 00:13:58 -0400

    Added /api/register endpoint for Flow enrollment

--- flow-server/routes/api.js
+++ flow-server/routes/api.js
@@ -1,6 +1,5 @@
 const createError = require('http-errors');
 const express = require('express');
-const passport = require('passport');
 const checkAuth = require('../util/check-auth');
 
 const router = express.Router();
@@ -8,16 +7,17 @@ const router = express.Router();
 /*
   POST data points for a Flow
     id (of the Flow in question, should be registered)
+    timestamp (should be a numerical representation)
     value (should be an int or float)
   timestamp is recorded on the server
 */
 router.post('/flow', (req, res, next) => {
   const { id } = req.query;
-  const { value } = req.body;
+  const { timestamp, value } = req.body;
 
   // make sure we have valid params
-  if (!id || !value || Number.isNaN(value)) {
-    next(createError(400, 'Provide a value Flow ID and data value'));
+  if (!id || !timestamp || !value || Number.isNaN(value)) {
+    return next(createError(400, 'Provide a Flow ID, timestamp, and value'));
   }
 
   // check for Flow membership in db
@@ -27,10 +27,41 @@ router.post('/flow', (req, res, next) => {
     if (!reply) return next(createError(404, 'Flow not found'));
 
     // insert data into sorted set for this Flow (include timestamp)
-    const timestamp = Date.now();
     req.db.zadd([`flow:${id}`, timestamp, value], (addErr) => {
       if (addErr) return next(createError(500, err));
       res.status(200).send(`Recorded data for Flow ${id} at ${timestamp}`);
+    });
+  });
+});
+
+/*
+  POST register a Flow
+    id (of the Flow in question, should be unregistered)
+    username (the Flow will be registered to this user)
+*/
+router.post('/register', checkAuth, (req, res, next) => {
+  const { id } = req.body;
+  const { username } = req.user;
+
+  // make sure we have valid params
+  if (!id || !username) {
+    return next(createError(400, 'Provide a Flow ID and username'));
+  }
+
+  // make sure user doesn't already have a Flow
+  if (req.user.flow) {
+    return next(createError(409, 'User already has a Flow registered'));
+  }
+
+  // make sure Flow is not registered already
+  req.db.sismember('flows', id, (err, reply) => {
+    if (err) return next(createError(500, err));
+    if (reply) return next(createError(409, 'Flow already registered'));
+
+    // enroll this Flow for the user
+    req.db.hset(`user:${username}`, 'flow', id, (regErr) => {
+      if (regErr) return next(createError(500, err));
+      res.status(200).send(`Registered Flow ${id} to ${username}`);
     });
   });
 });

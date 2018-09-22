fceb4c878fed7a7fb28af7700cfa4a5641dc077e
Ashwin Srinivasan <fishdev@users.noreply.github.com>
2018-09-22 13:21:28 -0400

    Linty

--- flow-server/util/smart-ai.js
+++ flow-server/util/smart-ai.js
@@ -17,12 +17,12 @@ function predict(data, time) {
   const regression = new PolynomialRegression(newData[0], newData[1], n);
 
   const numIntervals = 20;
-  const lastTime = parseInt(newData[0][n - 1]);
+  const lastTime = Number.parseInt(newData[0][n - 1], 10);
   const interval = (time - lastTime) / numIntervals;
-  
+
   const outputData = [];
   for (let i = 1; i <= numIntervals; i += 1) {
-    const xVal = parseInt(lastTime + i * interval);
+    const xVal = Number.parseInt(lastTime + i * interval, 10);
     outputData[i - 1] = {
       x: xVal,
       y: regression.predict(xVal),
@@ -31,9 +31,19 @@ function predict(data, time) {
   return outputData;
 }
 
-//console.log(predict([{ x: '1537617195', y: '1' }, { x: '1537617224', y: '4' }, { x: '1537617233', y: '2.5' }], 1538236733));
+// Converts array of timestamps and values into {x, y}
+function toPlotPoints(arr) {
+  const data = [];
+  for (let i = 0; i < arr.length - 1; i += 2) {
+    data.push({
+      x: arr[i + 1],
+      y: arr[i],
+    });
+  }
+  return data;
+}
 
 module.exports = {
   toPlotPoints,
-  predict2,
+  predict,
 };

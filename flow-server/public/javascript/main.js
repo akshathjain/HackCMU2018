859e8c8d970de70c76cbed140a893a5920f7e1c9
soumilm <soumilm@andrew.cmu.edu>
2018-09-22 01:27:48 -0400

    Attempt at signup functionality

--- flow-server/public/javascript/main.js
+++ flow-server/public/javascript/main.js
@@ -1,9 +1,13 @@
+var passwordVisible = false;
+
+// Nice Scrolling
 function scrollToSection(id) {
   document.getElementById(id).scrollIntoView({
     behavior: 'smooth',
   });
 }
 
+// Show or hide LogIn/SignUp popups
 function showModal(id, displayVal) {
   if (displayVal) {
     $(`#${id}`).css('display', 'inherit');
@@ -13,16 +17,48 @@ function showModal(id, displayVal) {
   }
 }
 
-function checkPassword() {
-  if ($('#passwordVal1').val() === '') {
-    $('.passwordMatch').addClass('is-hidden');
-    return true;
+// Make password visible I think
+function togglePWVisibility() {
+  if (passwordVisible) {
+    $("#PWVisible").addClass("fa-eye");
+    $("#PWVisible").removeClass("fa-eye-slash");
+    document.getElementById('signUpPassword').type = "password";
+    passwordVisible = false;
   }
-  console.log($('#passwordVal1').val(), $('#passwordVal0').val());
-  if ($('#passwordVal0').val() !== $('#passwordVal1').val()) {
-    $('.passwordMatch').removeClass('is-hidden');
-    return false;
+  else {
+    $("#PWVisible").removeClass("fa-eye");
+    $("#PWVisible").addClass("fa-eye-slash");
+    document.getElementById('signUpPassword').type = "text";
+    passwordVisible = true;
   }
-  $('.passwordMatch').addClass('is-hidden');
-  return true;
+}
+
+function checkUName(username) {
+  if (username !== "") {
+    console.log(username);
+  }
+}
+
+function signUp() {
+  var username = $('#signUpUserName').val();
+  var email = $('#signUpEmail').val();
+  var password = sha512($('#signUpPassword').val());
+  
+  $.post("/login/signup/", 
+    {
+      "username": username, 
+      "email": email, 
+      "password": password
+    }, 
+    function(data, status){
+      alert("Data: " + data + "\nStatus: " + status);
+      //alert("Hello");
+    }
+  );
+}
+
+function logIn() {
+  var username = $('#logInUserName').val();
+  var password = $('#logInPassword').val();
+  doSomething(username, email, password);
 }
